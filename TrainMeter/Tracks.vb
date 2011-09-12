8998e4456594fc08951f607d8780b531bdcabe39
Pedro Silva <pemiolsi@gmail.com>
2011-09-12 13:47:30 +0100

    API now supports editing

--- TrainMeter/Tracks.vb
+++ TrainMeter/Tracks.vb
@@ -5,6 +5,14 @@
         Return QAT.Execute("LIST ID, name, size FROM tracks WHERE PROFILE_ID = @profile@")
     End Function
 
+    Public Function EditTrack(ByVal TrackID As Integer, ByVal Name As String, ByVal Distance As Single) As Boolean
+        QAT.AddParameter(New QATDB.QATCore.QATParameter("ID", TrackID))
+        QAT.AddParameter(New QATDB.QATCore.QATParameter("name", Name))
+        QAT.AddParameter(New QATDB.QATCore.QATParameter("size", Distance))
+        QAT.Execute("CHANGE name=@name@,size=@size@ FROM tracks WHERE ID = @ID@")
+        Return True
+    End Function
+
     Public Function AddTrack(ByVal profile As Integer, ByVal name As String, ByVal size As Double) As Boolean
         QAT.AddParameter(New QATDB.QATCore.QATParameter("profile", profile))
         QAT.AddParameter(New QATDB.QATCore.QATParameter("name", name))
